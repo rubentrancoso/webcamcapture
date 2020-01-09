d82e5da7d3055f54cbba8b1f570a36b020465d6a
rubentrancoso <rubentrancoso@gmail.com>
2020-01-09 16:39:03 -0300

    some positioning hacks

--- src/main/java/WebcamTest.java
+++ src/main/java/WebcamTest.java
@@ -15,57 +15,97 @@ public class WebcamTest extends JFrame implements MouseListener, MouseMotionList
 
 	private static final long serialVersionUID = 1L;
 	private static boolean moveMode = false;
-	private static Point location  = new Point(0,0);
-	private static Point mousePos = new Point(0,0);
+	private static Point location = new Point(0, 0);
+	private static Point mousePos = new Point(0, 0);
+	private static Dimension camSize = new Dimension(176, 144);
+	private static int gripSize = 20;
+
+	public WebcamTest(WebcamPanel panel, double factor) {
+		if (factor <= 0) {
+			factor = 1;
+		}
+		this.buildPanel(panel);
+		int width = (int) (this.getWidth() * factor);
+		int height = (int) (this.getHeight() * factor);
+		this.setSize(width, height);
+		this.setLocation();
+	}
 
 	public WebcamTest(WebcamPanel panel) {
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
-		int taskBarSize = scnMax.bottom;
+		this.buildPanel(panel);
+		this.setLocation();
+	}
+
+	private void buildPanel(WebcamPanel panel) {
 		this.add(panel);
 		this.setResizable(false);
-		this.setSize(new Dimension(176, 144));
+//		this.setSize(new Dimension(176, 144));
+//		this.setSize(new Dimension(100, 100));
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setUndecorated(true);
 		this.setAlwaysOnTop(true);
 		this.pack();
-		location.setLocation(screenSize.width - this.getWidth(), screenSize.height - taskBarSize - this.getHeight());
-		this.setLocation(location);
 		this.setVisible(true);
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		Webcam webcam = Webcam.getDefault();
-		webcam.setViewSize(new Dimension(176, 144));
-		WebcamPanel panel = new WebcamPanel(webcam);
-		panel.setMirrored(true);
-		new WebcamTest(panel);
+	private void setLocation() {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
+		int taskBarSize = scnMax.bottom;
+		location.setLocation(screenSize.width - this.getWidth(), screenSize.height - taskBarSize - this.getHeight());
+		this.setLocation(location);
 	}
 
-	public void mouseClicked(MouseEvent e) {
-		if (e.getClickCount() == 2 && !e.isConsumed()) {
-			e.consume();
-			if(e.getX() > this.getWidth() - 10 && e.getY() < 10) {
-				System.exit(0);
-			} else {
-				Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-				Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
-				int taskBarSize = scnMax.bottom;
-				location.setLocation(screenSize.width - this.getWidth(), screenSize.height - taskBarSize - this.getHeight());
-				this.setLocation(location);				
-			}
-		}
+	public static void main(String[] args) throws InterruptedException 
+	{
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(camSize);
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setMirrored(true);
+		new WebcamTest(panel, .8);
+	}
+
+	public void mouseClicked(MouseEvent e) 
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
+		int taskBarSize = scnMax.bottom;
 		
+		if (e.getClickCount() == 2 && !e.isConsumed()) 
+		{
+			e.consume();
+			if (e.getY() < gripSize && e.getX() > this.getWidth() - gripSize) 
+			{
+				location.setLocation(screenSize.width - this.getWidth(), 0);
+				this.setLocation(location);
+			} 
+			if (e.getY() < gripSize && e.getX() < gripSize) 
+			{
+				location.setLocation(0, 0);
+				this.setLocation(location);
+			} 
+			if (e.getY() > this.getHeight() - gripSize && e.getX() < gripSize) 
+			{
+				location.setLocation(0, screenSize.height - this.getHeight() - taskBarSize);
+				this.setLocation(location);
+			} 
+			if (e.getY() > this.getHeight() - gripSize && e.getX() > this.getWidth() - gripSize ) 
+			{
+				location.setLocation(screenSize.width - this.getWidth(), screenSize.height - this.getHeight() - taskBarSize);
+				this.setLocation(location);
+			} 
+		}
+
 	}
 
 	public void mouseDragged(MouseEvent e) {
 		if (moveMode) {
-			this.setLocation(e.getXOnScreen() - (int)mousePos.getX(),e.getYOnScreen() - (int)mousePos.getY());
+			this.setLocation(e.getXOnScreen() - (int) mousePos.getX(), e.getYOnScreen() - (int) mousePos.getY());
 		}
-	}	
-	
+	}
+
 	public void mousePressed(MouseEvent e) {
 		moveMode = true;
 		mousePos.setLocation(e.getX(), e.getY());
