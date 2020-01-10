5c2824d59703f90593636ec2c7c25a94db0fb852
rubentrancoso <rubentrancoso@gmail.com>
2020-01-10 17:54:00 -0300

    use automenu package from github

---
+++ packit.bat
@@ -0,0 +1,5 @@
+echo on
+jar cmf src\META-INF\MANIFEST.MF picinpic.jar WebcamTest.java
+REM call mvn dependency:resolve
+REM call mvn install
+REM call mvn package -DskipTests -U
--- src/main/java/com/tivit/component/WebcamTest.java
+++ src/main/java/com/tivit/component/WebcamTest.java
@@ -1,3 +1,5 @@
+package com.tivit.component;
+
 import java.awt.Dimension;
 import java.awt.Insets;
 import java.awt.Point;
@@ -5,35 +7,55 @@ import java.awt.Toolkit;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+
+import org.digitalmonks.automenu.APopupMenu;
+import org.digitalmonks.automenu.control.events.AAutoMenuEvent;
+import org.digitalmonks.automenu.control.events.AAutoMenuListener;
 
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 
-public class WebcamTest extends JFrame implements MouseListener, MouseMotionListener {
+public class WebcamTest extends JFrame implements MouseListener, MouseMotionListener, AAutoMenuListener {
 
-	private static final long serialVersionUID = 1L;
-	private static boolean moveMode = false;
+	private static final long serialVersionUID = 8291705595974153605L;
+	private static boolean moveMode;
 	private static Point location = new Point(0, 0);
 	private static Point mousePos = new Point(0, 0);
-	private static Dimension camSize = new Dimension(176, 144);
+	private static Dimension camSize = new Dimension(640, 480);  // [176x144] [320x240] [640x480] 
 	private static int gripSize = 20;
+	@SuppressWarnings("unused")
+	private APopupMenu apopupmenu;
+	File temp = new File("D:\\");
 
 	public WebcamTest(WebcamPanel panel, double factor) {
 		if (factor <= 0) {
 			factor = 1;
 		}
 		this.buildPanel(panel);
+		this.buildMenu();
 		int width = (int) (this.getWidth() * factor);
 		int height = (int) (this.getHeight() * factor);
 		this.setSize(width, height);
 		this.setLocation();
+		this.setVisible(true);
 	}
 
 	public WebcamTest(WebcamPanel panel) {
 		this.buildPanel(panel);
+		this.buildMenu();
 		this.setLocation();
+		this.setVisible(true);
+	}
+
+	private void buildMenu() {
+		String xmlFile;
+		xmlFile = "popmenu2.xml";
+		apopupmenu = new APopupMenu(xmlFile, this, this);
 	}
 
 	private void buildPanel(WebcamPanel panel) {
@@ -45,7 +67,6 @@ public class WebcamTest extends JFrame implements MouseListener, MouseMotionList
 		this.setUndecorated(true);
 		this.setAlwaysOnTop(true);
 		this.pack();
-		this.setVisible(true);
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 	}
@@ -58,44 +79,38 @@ public class WebcamTest extends JFrame implements MouseListener, MouseMotionList
 		this.setLocation(location);
 	}
 
-	public static void main(String[] args) throws InterruptedException 
-	{
+	public static void main(String[] args) throws InterruptedException {
 		Webcam webcam = Webcam.getDefault();
 		webcam.setViewSize(camSize);
 		WebcamPanel panel = new WebcamPanel(webcam);
 		panel.setMirrored(true);
-		new WebcamTest(panel, .8);
+		new WebcamTest(panel, .3);
 	}
 
-	public void mouseClicked(MouseEvent e) 
-	{
+	public void mouseClicked(MouseEvent e) {
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		Insets scnMax = Toolkit.getDefaultToolkit().getScreenInsets(this.getGraphicsConfiguration());
 		int taskBarSize = scnMax.bottom;
-		
-		if (e.getClickCount() == 2 && !e.isConsumed()) 
-		{
+
+		if (e.getClickCount() == 2 && !e.isConsumed()) {
 			e.consume();
-			if (e.getY() < gripSize && e.getX() > this.getWidth() - gripSize) 
-			{
+			if (e.getY() < gripSize && e.getX() > this.getWidth() - gripSize) {
 				location.setLocation(screenSize.width - this.getWidth(), 0);
 				this.setLocation(location);
-			} 
-			if (e.getY() < gripSize && e.getX() < gripSize) 
-			{
+			}
+			if (e.getY() < gripSize && e.getX() < gripSize) {
 				location.setLocation(0, 0);
 				this.setLocation(location);
-			} 
-			if (e.getY() > this.getHeight() - gripSize && e.getX() < gripSize) 
-			{
+			}
+			if (e.getY() > this.getHeight() - gripSize && e.getX() < gripSize) {
 				location.setLocation(0, screenSize.height - this.getHeight() - taskBarSize);
 				this.setLocation(location);
-			} 
-			if (e.getY() > this.getHeight() - gripSize && e.getX() > this.getWidth() - gripSize ) 
-			{
-				location.setLocation(screenSize.width - this.getWidth(), screenSize.height - this.getHeight() - taskBarSize);
+			}
+			if (e.getY() > this.getHeight() - gripSize && e.getX() > this.getWidth() - gripSize) {
+				location.setLocation(screenSize.width - this.getWidth(),
+						screenSize.height - this.getHeight() - taskBarSize);
 				this.setLocation(location);
-			} 
+			}
 		}
 
 	}
@@ -122,6 +137,37 @@ public class WebcamTest extends JFrame implements MouseListener, MouseMotionList
 	}
 
 	public void mouseMoved(MouseEvent e) {
+	}
+
+	public void receiveAutoMenuEvents(AAutoMenuEvent e) {
+		switch (e.getType()) 
+		{
+		case AAutoMenuEvent.EVENTTYPE_POPUPMENU:
+		default:
+	        if ("10.00".equals(((JMenuItem) e.getSource()).getActionCommand()))
+	        {
+			  System.exit(0);
+			}			
+	        if ("10.01".equals(((JMenuItem) e.getSource()).getActionCommand()))
+	        {
+			  // Start	
+
+	      
+			}			
+	        if ("10.02".equals(((JMenuItem) e.getSource()).getActionCommand()))
+	        {
+	        	// Stop
+
+			}			
+		}		
+	}
+
+	public void frameRecorded(boolean arg0) throws IOException {
+	
+	}
+
+	public void recordingStopped() {
+
 	}
 
 }
